22edbe3980778082525cfbd00f16364976749c34
rocketpy <dimitr.ushakov@gmail.com>
2020-08-21 17:43:31 +0300

    added Options

--- conftest.py
+++ conftest.py
@@ -35,6 +35,20 @@ class TestLogin:
         # this test will be run 2 times
 """
 
+"""
+#  languages , {accept-language: ru, en}
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+#  for Firefox
+fp = webdriver.FirefoxProfile()
+fp.set_preference("intl.accept_languages", user_language)
+browser = webdriver.Firefox(firefox_profile=fp)
+"""
+
 # for use this fixture in file  test_file_name.py :
 """
 link = "http://..."
