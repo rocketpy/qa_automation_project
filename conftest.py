a5fe570ce332efc26853a298b2478632a4e073e7
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-21 16:57:58 +0300

    added gitignore , conftest files

---
+++ conftest.py
@@ -0,0 +1,36 @@
+#  here we put our fixture
+#  Можно создавать дополнительные файлы conftest.py в других директориях, но тогда настройки в этих файлах будут применяться только к тестам в под-директориях.
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+    
+#  now we can use fixture in ANY TEST !!!
+# in file  test_conftest.py , like example:
+"""
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+"""
+
+#  structure must be like:
+"""
+selenium_course_solutions/
+├── section3
+│   └── conftest.py
+│   └── test_languages.py
+├── section4 
+│   └── conftest.py
+│   └── test_main_page.py
+
+"""
