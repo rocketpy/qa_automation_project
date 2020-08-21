bf51890de1c7f875d8514c4e6e98b078c5d46875
rocketpy <dimitr.ushakov@gmail.com>
2020-08-21 17:20:40 +0300

    added a main code

--- conftest.py
+++ conftest.py
@@ -1,6 +1,3 @@
-#  here we put our fixture
-#  Можно создавать дополнительные файлы conftest.py в других директориях, но тогда настройки в этих файлах будут применяться только к тестам в под-директориях.
-
 import pytest
 from selenium import webdriver
 
@@ -13,24 +10,36 @@ def browser():
     print("\nquit browser..")
     browser.quit()
     
-#  now we can use fixture in ANY TEST !!!
-# in file  test_conftest.py , like example:
-"""
-link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_should_see_login_link(browser):
+"""
+#  pytest -s -v test_file_name.py
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
 """
 
-#  structure must be like:
 """
-selenium_course_solutions/
-├── section3
-│   └── conftest.py
-│   └── test_languages.py
-├── section4 
-│   └── conftest.py
-│   └── test_main_page.py
+#  for all class
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        # this test will be run 2 times
 
+    def test_guest_should_see_navbar_element(self, browser, language):
+        # this test will be run 2 times
+"""
+
+# for use this fixture in file  test_file_name.py :
+"""
+link = "http://..."
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
 """
