8e37f3e8d65ae4ac8279b5f6beab571b78ac8304
rocketpy <dimitr.ushakov@gmail.com>
2020-08-23 00:38:38 +0300

    modified base method

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,13 @@
+from .pages.main_page import MainPage  #  from pages.main_page import MainPage
 import pytest
 from selenium import webdriver
+#  from pages.base_page import BasePage
 
 
 #  pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)  
+    page.open()                      
+    page.go_to_login_page()  
