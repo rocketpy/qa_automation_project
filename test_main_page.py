8cf264fd4486dea9f4bb6d0350505bff53f94a3a
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-23 23:44:29 +0300

    added new method test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
-from pages.main_page import MainPage  # from pages.main_page import MainPage
+from .pages.main_page import MainPage
+from pages.base_page import BasePage
 import pytest
 from selenium import webdriver
-#  from pages.base_page import BasePage
 
 
 #  pytest -v --tb=line --language=en test_main_page.py
@@ -11,3 +11,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  
     page.open()                      
     page.go_to_login_page()  
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
