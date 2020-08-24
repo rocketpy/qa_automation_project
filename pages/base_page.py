b75bc8fa43fa1e12bfb18a2381f43c2a3558d20e
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-24 23:42:17 +0300

    added in constructor a new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,3 +7,10 @@ class BasePage:
         
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except:
+            return False
+        return True
