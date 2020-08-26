6afff44ea08dd10356ed31e8d8e9f4533bb559de
rocketpy <dimitr.ushakov@gmail.com>
2020-08-26 23:46:31 +0300

    added a modified method

--- pages/main_page.py
+++ pages/main_page.py
@@ -19,3 +19,8 @@ class MainPage(BasePage):
 # replace where #login_link
 #def should_be_login_link(self):
 #    assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url) 
