421fe744f17731b734321e9356cdd433573b2555
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-27 23:00:00 +0300

    modified

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
-from .login_page import LoginPage
+# from .locators import MainPageLocators
+# from .login_page import LoginPage
 
 
 class MainPage(BasePage): 
@@ -13,14 +13,16 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
 
+
 # def should_be_login_link(self):
 #    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
 # replace where #login_link
-#def should_be_login_link(self):
+# def should_be_login_link(self):
 #    assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
+"""
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
         return LoginPage(browser=self.browser, url=self.browser.current_url) 
+"""
--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@ import pytest
 from selenium import webdriver
 
 
+# #  pytest -v --tb=line test_main_page.py
 #  pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,3 +19,4 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
