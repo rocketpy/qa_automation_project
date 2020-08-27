84ae88902c59e25a8a4c32480bc6ebc2df6a9f13
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-27 23:39:29 +0300

    modified methods

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-# from .locators import MainPageLocators
+from .locators import MainPageLocators
 # from .login_page import LoginPage
 
 
@@ -10,12 +10,11 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+#    def should_be_login_link(self):
+#        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-
-
-# def should_be_login_link(self):
-#    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
 # replace where #login_link
 # def should_be_login_link(self):
