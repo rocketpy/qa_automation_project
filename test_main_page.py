3d6317054984d88c4482cc1542af2649b4304bb2
rocketpy <dimitr.ushakov@gmail.com>
2020-08-28 23:15:35 +0300

    updated

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from pages.main_page import MainPage
 from pages.base_page import BasePage
+from .pages.login_page import LoginPage
 import pytest
 from selenium import webdriver
 
@@ -11,7 +12,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
