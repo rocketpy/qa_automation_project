09c18609062138f2730e38fc05e14bcf15fd1c3e
rocketpy <dimitr.ushakov@gmail.com>
2020-08-28 23:14:08 +0300

    updated

--- test_main_page.py
+++ test_main_page.py
@@ -8,15 +8,14 @@ from selenium import webdriver
 # pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  
-    page.open()                      
-    page.go_to_login_page()  
-
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
