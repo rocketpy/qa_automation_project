b3d2ceb596a97ea461912e0f1c16017b5a51091a
rocketpy <dimitr.ushakov@gmail.com>
2020-08-28 23:25:28 +0300

    updated

--- pages/main_page.py
+++ pages/main_page.py
@@ -13,10 +13,9 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
         # assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
-"""
+        
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element_by_css_selector("#login_link")
         link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url) 
-"""
+        alert = self.browser.switch_to.alert
+        alert.accept()
