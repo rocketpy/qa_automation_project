6f3682b4b60c95dc7053f41677a79b26d6c002c2
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-28 18:46:07 +0300

    updated methods

--- test_main_page.py
+++ test_main_page.py
@@ -8,16 +8,14 @@ from selenium import webdriver
 # pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  
     page.open()                      
     page.go_to_login_page()  
 
 
 def test_guest_should_see_login_link(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
