1f8f274e18296ff5ea055e11857dec694dde65f6
rocketpy <dimitr.ushakov@gmail.com>
2020-08-28 23:00:40 +0300

    full updated

--- pages/locators.py
+++ pages/locators.py
@@ -1,34 +1,29 @@
-from .base_page import BasePage
-from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 
-class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group .btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
-    def should_be_login_url(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "Login url is not presented"
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
-        login_link.click()
-        assert "login" in self.browser.current_url, "There is not login in url"
-        
+class  LoginPageLocators():
+    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-        
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    BOOK_NAME_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
+    BOOK_PRICE_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(3) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
 
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-
-    def register_new_user(self, email, password):        
-        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
-        email_field.send_keys(email)
-        password_field1 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_1)
-        password_field1.send_keys(password)
-        password_field2 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_2)
-        password_field2.send_keys(password)
-        button_submit = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
-        button_submit.click()
+class BasketPageLocators():
+    EMPTY_BASKET = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
