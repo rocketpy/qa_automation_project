a553a3ebc9fade9f128377911c381d5265dfc7ba
rocketpy <dimitr.ushakov@gmail.com>
2020-08-29 21:57:59 +0300

    added a new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -17,3 +17,8 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+    
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+    
+    
