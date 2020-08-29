9cc5c186f908131ef413584d4fe6e4f550cfab5a
rocketpy <dimitr.ushakov@gmail.com>
2020-08-29 21:59:21 +0300

    added a new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -20,5 +20,8 @@ class BasePage:
     
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-    
-    
+        
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+        assert "login" in self.browser.current_url, "Link is invalid"
