28bf2943662d9f6a4a324de6316665ce17198f93
rocketpy <dimitr.ushakov@gmail.com>
2020-08-30 22:26:19 +0300

    added a new methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,1 +1,28 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from .login_page import LoginPage
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoAlertPresentException
+
+
+class ProductPage(BasePage):
+  
+    def add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), "Button is not presented"
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        button.click()
+  
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
