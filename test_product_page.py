c1a6f08833b1f424b528aeed090d077783b3a9f3
rocketpy <dimitr.ushakov@gmail.com>
2020-08-30 22:26:35 +0300

    added a new method

--- test_product_page.py
+++ test_product_page.py
@@ -9,16 +9,18 @@ from selenium import webdriver
 # pytest -s test_foo.py
 # pytest -v --tb=line test_main_page.py
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-
+    
+"""
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+"""
