effc65c0c353ba4f2be59ed7a181210a6a8ee2bb
rocketpy <dimitr.ushakov@gmail.com>
2020-08-31 23:31:03 +0300

    added a new class (BasketPage)

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,3 +3,9 @@ from .locators import BasketPageLocators
 from selenium.webdriver.common.by import By
 
 
+class BasketPage(BasePage):
+    def should_be_basket_empty_message(self):
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), "Item in basket"
+
+    def is_basket_empty(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket is not empty"
