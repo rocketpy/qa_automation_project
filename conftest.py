08743efe5504c0b86d4e7370bfe9292b149f77a1
rocketpy <dimitr.ushakov@gmail.com>
2020-09-01 22:02:37 +0300

    added a new function

--- conftest.py
+++ conftest.py
@@ -18,7 +18,26 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en")
     
     
-"""
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options )
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit() 
+    
+
 #  pytest -s -v test_file_name.py
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
@@ -26,7 +45,7 @@ def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
-"""
+
 
 """
 #  for all class
