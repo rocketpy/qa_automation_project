2726f6f01fa471c370ec08c4fad22ed000b9beab
rocketpy <dimitr.ushakov@gmail.com>
2020-09-04 21:11:15 +0300

    added a new function

--- test_product_page.py
+++ test_product_page.py
@@ -42,3 +42,10 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message()
     product_page.should_be_book_price()
     
+@pytest.mark.xfail(reason="not disappiring")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.is_dissappeared()    
