e28f974120ce82fb0248d3e5a7b0ad4ddb216d76
rocketpy <dimitr.ushakov@gmail.com>
2020-09-05 20:26:28 +0300

    added a new function

--- test_product_page.py
+++ test_product_page.py
@@ -63,3 +63,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
     
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_enter_basket()
+    page2 = BasketPage(browser, browser.current_url)
+    page2.is_basket_empty()
+    page2.should_be_basket_empty_message()
+    
+
+    
