b129fbde411a7955af375fe138e3e6e6ccce72e7
rocketpy <dimitr.ushakov@gmail.com>
2020-09-07 22:43:40 +0300

    added a new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -51,3 +51,10 @@ class BasePage:
         except NoAlertPresentException:
             print("No second alert presented")
             
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+    
