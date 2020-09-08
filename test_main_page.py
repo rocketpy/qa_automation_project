9c917de545cda9df48fe7218d05f43c4bc50a4f2
rocketpy <dimitr.ushakov@gmail.com>
2020-09-08 21:49:05 +0300

    modified imports

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
 from pages.base_page import BasePage
-from .pages.login_page import LoginPage
-from .pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 import pytest
 from selenium import webdriver
 
