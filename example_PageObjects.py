31b5183674e3f805e85d72c61ea1b6c5527781f4
rocketpy <dimitr.ushakov@gmail.com>
2020-09-25 21:47:54 +0300

    added Classes

--- example_PageObjects.py
+++ example_PageObjects.py
@@ -1,1 +1,21 @@
+class Page(object):
+    html = "html"
+    ok_button = "#ok"
+    cancel_button = "#cancel"
+    see_items_button = "button.items"
 
+
+class HomePage(object):
+    see_items_button = "button.items"
+
+
+class ShoppingPage(object):
+    buyable_item = 'img[alt="Item"]'
+    add_to_cart = "button.add"
+    go_to_checkout = "#checkout"
+
+
+class CheckoutPage(object):
+    remove_from_cart = "button.remove"
+    buy_now = "#buy-now"
+    shop_more = "#shop-more"
